4d0a155e5c2a71052e1b8c1227e56e5d84240ddf
Mostafa Diaa <diaa_sad8@icloud.com>
2018-04-23 19:06:44 +0200

    Initial Commit

---
+++ incubators/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  incubators
+//
+//  Created by Mostafa Diaa on 4/23/18.
+//  Copyright © 2018 Mostafa Diaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
